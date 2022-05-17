b910d3d317046f0cdf2f6203256974e4400b025a
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-05-17 16:25:56 -0300

    Make script runnable and add sleep time

--- osticket-ping.py
+++ osticket-ping.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
 import os
 import activesoup
+import time
 
 LOGIN = os.environ.get('LOGIN')
 PASSWORD = os.environ.get('PASSWORD')
+SLEEP_DURATION = 20
 
 d = activesoup.Driver()
 page = d.get("https://suporteic.ufba.br/scp/login.php")
@@ -14,3 +18,5 @@ print(2)
 page = d.get("https://suporteic.ufba.br/scp/autocron.php")
 print(3)
 print(page._raw_response)
+
+time.sleep(SLEEP_DURATION)
